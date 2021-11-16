71da95a3ce631b6714759d69877a53ab5fa1a249
RestuNugel <94230412+RestuNugel@users.noreply.github.com>
2021-11-16 23:44:54 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,16 +1,17 @@
-let handler = function (m) {
-  // this.sendContact(m.chat, '6289526350998', 'Nurutomo', m)
-  let contacts = []
-  for (let a of Object.keys(Owner)) {
-let owner = Owner[a]
-    contacts.push(...[a, owner.name])
+function handler(m) {
+   let listOwner = new Array()
+  for (let i of owner.map(v => v.replace(/\D/g, '') + '@s.whatsapp.net')) {
+  	listOwner.push({ vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${this.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Work\nURL;Web gwejh: http://github.com/Rlxfly\nEMAIL;y: ${this.user.name}@gmail.com\nORG: BUKAN BOT + NO SAVE\nTEL;NO CHAT P ANJG;waid= ${i.split('@')[0]}:${i.split('@')[0]}\nEND:VCARD` })
   }
-  if (contacts.length < 1) return this.sendContact(m.chat, contacts[0], contacts[1], m)
-  this.sendContactArray(m.chat, contacts, m)
+  let send = this.sendMessage(m.chat, { displayName: listOwner.length + ' kontak', contacts: listOwner }, 'contactsArrayMessage', { quoted: m })
+return m.reply('Chat owner *P, & sv*\n\n *Ga ada sopan santun*')
 }
+
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
-
 handler.command = /^(owner|creator)$/i
 
 module.exports = handler
+
+
+//PA PE PA PE SV sv sv NGTD LU
